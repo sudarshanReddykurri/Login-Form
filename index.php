7b3fccc31a27030652cbdb08c9a79bdc455452f0
Andrew Stewart <andrew@averagestudios.com>
2011-11-23 22:27:52 -0800

    Removed trailing whitespace, and fixed elements to be HTML5 compatible:
    Example:
    
      <br /> becomes <br>

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html> 
-<html lang="en"> 
+<!DOCTYPE html>
+<html lang="en">
     <head>
         <title>Login Page</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
-        <meta name="description" content="Simple Login Form"  /> 
-        <link rel="stylesheet" type="text/css" href="css/reset.css" />
-        <link rel="stylesheet" type="text/css" href="css/styles.css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="description" content="Simple Login Form">
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
 
         <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js?ver=3.2.1'></script>
         <script type='text/javascript' src='http://demo5.launcheffectapp.com/wp-content/themes/launcheffect/js/init.js?ver=1.0'></script>
@@ -27,21 +27,21 @@
     </head>
 
     <body style="background: url(images/bkg_body.gif)  repeat-x white;">
-        <div id="outer-container" style="background: url(images/bg1.gif) top center no-repeat;"> 
-            <div id="container"> 
-                <div id="inner-container"> 
+        <div id="outer-container" style="background: url(images/bg1.gif) top center no-repeat;">
+            <div id="container">
+                <div id="inner-container">
                     <h1 style="margin-left: 8px;">Login Page</h1>
                     <div id="content-blocks-wrapper">
                         <form action="index.php" method="POST" name="ticket">
                             <fieldset>
                                 <label for="accountname" style="margin-left: 10px;">USERNAME:</label>
-                                <input name="accountname" type="text"/>
+                                <input name="accountname" type="text">
                                 <label for="password" style="margin-top: -26px; margin-left: 255px;">PASSWORD:</label>
-                                <input name="password" type="password"/>
+                                <input name="password" type="password">
                                 <div class="clear"></div>
-                                <span id="submit-button-border" style="margin-left: 10px;"><input type="submit" name="submit" value="Login" id="submit-button"  /></span>
+                                <span id="submit-button-border" style="margin-left: 10px;"><input type="submit" name="submit" value="Login" id="submit-button"  ></span>
                                 <div class="clear"></div>
-                                <br />
+                                <br>
                             </fieldset>
                         </form>
                         <?php
@@ -74,20 +74,20 @@
                                 echo "<script> window.location = \"http://google.ca\"; </script>";
                                 exit;
                             } else {
-                                echo "<span style= \"color:#ff5e60; margin-left:10px;\">Username or password is incorrect.</span><br /><br />";
+                                echo "<span style= \"color:#ff5e60; margin-left:10px;\">Username or password is incorrect.</span><br><br>";
                             }
                         }
                         ?>
                         <span class="register" style="margin-left: 10px;">Register for an account <a href="registration.php">here</a>.</span>
                     </div><!-- end #content-blocks-wrapper -->
-                    <div class="clear"></div>				
-                </div> 
-            </div> 
-        </div> 
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
         <ul id="footer" style="padding-bottom: 7px;">
             <li>
                 Page development by <a href="http://mnmlly.com" style="text-decoration: none; color:#58d1ef;!important">Jason Lennstrom</a>.
             </li>
-        </ul>						
+        </ul>
     </body>
 </html>
